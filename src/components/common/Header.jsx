020c5a6d111c23bf2e7df0737d3c8a7df2f182cd
mohanreddychirra <mohanreddy1594@gmail.com>
2019-04-15 19:43:33 +0100

    fixed header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,81 +5,84 @@ import NavBar from './NavBar';
 import { logout } from '../../actions/authAction';
 
 const Header = ({ orders, request, cart, auth, logout }) => (
-  <div id="header">
-    <div className="aligner clearfix">
-      <Link to='/'>
-        <span className="page-name">
+  <Fragment>
+    <div id="header">
+      <div className="aligner clearfix">
+        <Link to='/'>
+          <span className="page-name">
+            {
+              auth.role === 'vendor' && request
+                ? request.shopName
+                : 'Wahkart'
+            }
+          </span>
+        </Link>
+        
+        <div id="nav-icon">
+          <span><i className="fas fa-bars" /></span>
+          <NavBar />
+        </div>
+
+        <div style={{ display: "none" }}>
           {
-            auth.role === 'vendor' && request
-              ? request.shopName
-              : 'Wahkart'
+            (!auth.id || !auth.role || !auth.email) && (
+              <Fragment>
+                <Link to="/login">
+                  <span className="navlink">LOGIN</span>
+                </Link>
+
+                <Link to="/register">
+                  <span className="navlink">REGISTER</span>
+                </Link>
+              </Fragment>
+            )
           }
-        </span>
-      </Link>
+
+          {
+            auth.id && auth.role && auth.email && (
+              <Fragment>
+                <Link to="#" onClick={logout}>
+                  <span className="navlink">LOGOUT</span>
+                </Link>
       
-      <div id="nav-icon">
-        <span><i className="fas fa-bars" /></span>
-        <NavBar />
-      </div>
+                <Link to={
+                  ['vendor', 'admin'].includes(auth.role)
+                    ? `/${auth.role}`
+                    : '#'
+                }>
+                  <div id="profile" className="clearfix">
+                    <img src="/images/avatar.png" alt="user avatar"/>
+                    <span>{ auth.email.split('@')[0] }</span>
+                  </div>
+                </Link>
+              </Fragment>
+            )
+          }
 
-      <div style={{ display: "none" }}>
-        {
-          (!auth.id || !auth.role || !auth.email) && (
-            <Fragment>
-              <Link to="/login">
-                <span className="navlink">LOGIN</span>
-              </Link>
+          { 
+            auth.role === 'customer' && (
+              <Fragment>
+                <Link to="/orders">
+                  <div id="order">
+                    <i className="fas fa-credit-card" />
+                    <span>{ orders.length }</span>
+                  </div>
+                </Link>
 
-              <Link to="/register">
-                <span className="navlink">REGISTER</span>
-              </Link>
+                <Link to="/cart">
+                  <div id="cart">
+                    <i className="fas fa-cart-arrow-down" />
+                    <span>{ cart.length }</span>
+                  </div>
+                </Link>
             </Fragment>
-          )
-        }
-
-        {
-          auth.id && auth.role && auth.email && (
-            <Fragment>
-              <Link to="#" onClick={logout}>
-                <span className="navlink">LOGOUT</span>
-              </Link>
-    
-              <Link to={
-                ['vendor', 'admin'].includes(auth.role)
-                  ? `/${auth.role}`
-                  : '#'
-              }>
-                <div id="profile" className="clearfix">
-                  <img src="/images/avatar.png" alt="user avatar"/>
-                  <span>{ auth.email.split('@')[0] }</span>
-                </div>
-              </Link>
-            </Fragment>
-          )
-        }
-
-        { 
-          auth.role === 'customer' && (
-            <Fragment>
-              <Link to="/orders">
-                <div id="order">
-                  <i className="fas fa-credit-card" />
-                  <span>{ orders.length }</span>
-                </div>
-              </Link>
-
-              <Link to="/cart">
-                <div id="cart">
-                  <i className="fas fa-cart-arrow-down" />
-                  <span>{ cart.length }</span>
-                </div>
-              </Link>
-          </Fragment>
-          )
-        }
+            )
+          }
+        </div>
       </div>
     </div>
-  </div>
+    <div id="header-spacer" />
+  </Fragment>
 );
 
 const mapStateToProps = (state) => ({
---
+++ src/components/common/Overlay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Overlay = () => (
+  <div id="page-overlay">
+    
+  </div>
+);
+
+const mapStateToProps = () => ({});
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
+
