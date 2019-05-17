e219cce69852b589d410549b0266817da7c9996d
mohanreddychirra <mohanreddy1594@gmail.com>
2019-05-17 21:29:49 +0100

    completed categories for admin

--- server/db/seeders/20190419205019-categories.js
+++ server/db/seeders/20190419205019-categories.js
@@ -8,9 +8,6 @@ module.exports = {
       },
       {
         name: 'Women Clothings'
-      },
-      {
-        name: 'Female Clothings'
       }
     ]);
   },
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -11,7 +11,7 @@ class Footer extends Component {
   render() {
     return (
       <div id="footer">
-  
+        
       </div>
     );
   }
